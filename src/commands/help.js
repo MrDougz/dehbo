63425cc3f571928732ceb27abb9aa4f77dbaa09e
MrDougz <mrdougz2006@gmail.com>
2020-07-26 10:10:03 -0300

    dm command help

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,5 +1,7 @@
 const execute = (bot, msg, args) => {
-  return msg.channel.send(`Hello, ${msg.author}! If you want to know all the commands that I have, visit my site! https://dehbo.app.netlify`)
+  return msg.channel.send(
+    `Hello, ${msg.author}! If you want to know all the commands that I have, visit my site! https://dehbo.app.netlify`
+  )
 }
 
 module.exports = {
