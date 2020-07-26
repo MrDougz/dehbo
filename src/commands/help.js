9c44ef0614dce9b92d201223d4bea140ee1e219b
MrDougz <mrdougz2006@gmail.com>
2020-07-26 16:40:25 -0300

    update help

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,10 +1,13 @@
 const execute = (bot, msg, args) => {
   let string = '====HELP====\n\n'
-
-  bot.descs.forEach((desc) => {
-    if (desc) {
-      string += `\`${process.env.prefix}${bot.mainName[desc]}: ${desc}\`\n`
-    }
+  bot.mainName.forEach((name) => {
+    bot.descs.forEach((desc) => {
+      if (desc) {
+        string += `\`${process.env.prefix}${bot.mainName.get(
+          name
+        )}: ${bot.descs.get(desc)}\`\n`
+      }
+    })
   })
 
   return msg.channel.send(`${msg.author},\n${string}`)
